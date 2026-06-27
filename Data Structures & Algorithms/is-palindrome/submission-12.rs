a1003d3797701a8636b3ba1c96741751d27c9a67
Cillian Smith <72135306+cillian-smith@users.noreply.github.com>
2026-06-27 09:39:33 +0100

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        // sanitize 
+        let cleaned: String = s.chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+        println!("{}", &cleaned);
+        let k : usize = cleaned.len();
+
+        for i in 0..k/2 {
+            println!("{:?}, {:?}", cleaned.chars().nth(i), cleaned.chars().nth(k-i-1));
+            if cleaned.chars().nth(i) != cleaned.chars().nth(k-i-1) {return false}
+        }
+        return true;
+
+    }
+}
